6052874141e1b47bfcb39ef37aaa994480d49dd2
David Ngo and Kevin Ahn <kevin@>
2018-05-30 11:15:20 -0700

    Add more scripts and styling for delete functionality and placing barks and meows into textboxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 $(function() {
   $("#meow").click(function() {
-    $("#bark").after("<p>bark</p>");
+    $("#bark").next().append("<p><img src=\"img/dog.jpg\" class=\"small\"> Woof<button class=\"delete\">Delete</button></p>");
+    $(".delete").click(function() {
+      $(this).parent().remove();
+    });
   });
   $("#bark").click(function() {
-    $("#meow").after("<p>Meow</p>");
+    $("#meow").next().append("<p><img src=\"img/cat.jpg\" class=\"small\"> Meow<button class=\"delete\">Delete</button></p>");
+    $(".delete").click(function() {
+      $(this).parent().remove();
+    });
   });
+
+
 });
